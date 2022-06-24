5fa82a3dacdc6db163bdb6f0fe117ecb771a4bf0
James Andrews <james.andrews@sennen.tech>
2022-06-24 09:03:51 +0100

    rebuild with new versions

--- gen/restapi/configure_soundslike.go
+++ gen/restapi/configure_soundslike.go
@@ -4,20 +4,16 @@ package restapi
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
 
-	"github.com/rs/cors"
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
 
-	errors "github.com/go-openapi/errors"
-	runtime "github.com/go-openapi/runtime"
-	middleware "github.com/go-openapi/runtime/middleware"
-
-	"github.com/vorsprung/soundslike"
 	"github.com/vorsprung/soundslike/gen/restapi/operations"
 )
 
-//go:generate swagger generate server --target ../../gen --name Soundslike --spec ../../swagger.yml
+//go:generate swagger generate server --target ../../gen --name Soundslike --spec ../../swagger.yml --principal interface{}
 
 func configureFlags(api *operations.SoundslikeAPI) {
 	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
@@ -26,35 +22,28 @@ func configureFlags(api *operations.SoundslikeAPI) {
 func configureAPI(api *operations.SoundslikeAPI) http.Handler {
 	// configure the api here
 	api.ServeError = errors.ServeError
-	var o soundslike.Phonic
-	o.Load("words.txt")
+
 	// Set your custom logger if needed. Default one is log.Printf
 	// Expected interface func(string, ...interface{})
 	//
 	// Example:
 	// api.Logger = log.Printf
 
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
 	api.JSONConsumer = runtime.JSONConsumer()
 
 	api.JSONProducer = runtime.JSONProducer()
-	api.ListWordsHandler = operations.ListWordsHandlerFunc(
-		func(params operations.ListWordsParams) middleware.Responder {
-			fmt.Printf(">>%v<<\n", params.Word)
-			// convert json object to string
-			arg := params.Word.(map[string]interface{})["word"].(string)
-			wordlistraw := o.SoundsLike(arg)
-			wordlist := make([]string, len(wordlistraw))
 
-			for ix, item := range wordlistraw {
-				wordlist[ix] = item
-			}
-			return operations.NewListWordsOK().WithPayload(wordlist)
-		})
 	if api.ListWordsHandler == nil {
 		api.ListWordsHandler = operations.ListWordsHandlerFunc(func(params operations.ListWordsParams) middleware.Responder {
-			return middleware.NotImplemented("operation .ListWords has not yet been implemented")
+			return middleware.NotImplemented("operation operations.ListWords has not yet been implemented")
 		})
 	}
+
+	api.PreServerShutdown = func() {}
 
 	api.ServerShutdown = func() {}
 
@@ -69,25 +58,18 @@ func configureTLS(tlsConfig *tls.Config) {
 // As soon as server is initialized but not run yet, this function will be called.
 // If you need to modify a config, store server instance to stop it individually later, this is the place.
 // This function can be called multiple times, depending on the number of serving schemes.
-// scheme value will be set accordingly: "http", "https" or "unix"
+// scheme value will be set accordingly: "http", "https" or "unix".
 func configureServer(s *http.Server, scheme, addr string) {
 }
 
 // The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
-// The middleware executes after routing but before authentication, binding and validation
+// The middleware executes after routing but before authentication, binding and validation.
 func setupMiddlewares(handler http.Handler) http.Handler {
 	return handler
 }
 
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
-// So this is a good place to plug in a panic handling middleware, logging and metrics
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	corsHandler := cors.New(cors.Options{
-		Debug:          false,
-		AllowedHeaders: []string{"*"},
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{},
-		MaxAge:         1000,
-	})
-	return corsHandler.Handler(handler)
+	return handler
 }
